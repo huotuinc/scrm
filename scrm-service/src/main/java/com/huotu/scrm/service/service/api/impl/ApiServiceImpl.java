68a936e5f68d7d87b34cf3c6f88aed96e8720fc2
helloztt <347871727@qq.com>
2017-07-12 19:32:41 +0800

    增加上传图片的service ; 补充 UserInterceptor

---
+++ scrm-service/src/main/java/com/huotu/scrm/service/service/api/impl/ApiServiceImpl.java
@@ -0,0 +1,44 @@
+package com.huotu.scrm.service.service.api.impl;
+
+import com.huotu.scrm.common.SysConstant;
+import com.huotu.scrm.common.httputil.HttpClientUtil;
+import com.huotu.scrm.common.utils.Constant;
+import com.huotu.scrm.service.entity.mall.Customer;
+import com.huotu.scrm.service.repository.CustomerRepository;
+import com.huotu.scrm.service.service.api.ApiService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by helloztt on 2017-07-12.
+ */
+@Service
+public class ApiServiceImpl implements ApiService {
+    private static final Log log = LogFactory.getLog(ApiServiceImpl.class);
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void userLogin(Long customerId, String redirectUrl) {
+        Customer customer = customerRepository.findOne(customerId);
+        if(customer == null){
+            return;
+        }
+        Map<String,String> requestMap = new HashMap<>();
+        requestMap.put("customerid", String.valueOf(customerId));
+        try {
+            redirectUrl = URLEncoder.encode(redirectUrl, Constant.UTF8);
+        } catch (UnsupportedEncodingException e) {
+            log.error("url encode error",e);
+        }
+        requestMap.put("redirecturl", redirectUrl);
+        HttpClientUtil.getInstance().get(customer.getSubDomain() + SysConstant.COOKIE_DOMAIN + "/OAuth2/XiangzhangAuthorize.aspx",requestMap);
+    }
+}
---
+++ scrm-web/src/main/java/com/huotu/scrm/web/service/impl/AbstractStaticResourceService.java
@@ -0,0 +1,100 @@
+package com.huotu.scrm.web.service.impl;
+
+import com.huotu.scrm.web.service.StaticResourceService;
+import com.huotu.scrm.web.service.VFSHelper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author CJ
+ */
+public abstract class AbstractStaticResourceService implements StaticResourceService {
+
+    private static final Log log = LogFactory.getLog(AbstractStaticResourceService.class);
+
+    protected URI uriPrefix;
+    protected URI fileHome;
+    protected URI huobanmallPrefix;
+//    protected URI ftpPrefix;
+//    protected URI ftpHome;
+    @Autowired
+    private VFSHelper vfsHelper;
+
+    @Override
+    public void deleteResource(String path) throws IOException {
+        if (path == null)
+            return;
+        StringBuilder stringBuilder = new StringBuilder(fileHome.toString());
+        if (!stringBuilder.toString().endsWith("/") && !path.startsWith("/"))
+            stringBuilder.append("/");
+        stringBuilder.append(path);
+
+        vfsHelper.handle(stringBuilder.toString(), FileObject::delete);
+    }
+
+    @Override
+    public URI uploadResource(String mode , String path, InputStream data) throws IOException, IllegalStateException, URISyntaxException {
+        StringBuilder stringBuilder;
+        if(this.huobanmallMode.equals(mode)){
+            stringBuilder = new StringBuilder(huobanmallPrefix.toString());
+        }else{
+            stringBuilder = new StringBuilder(fileHome.toString());
+        }
+        if (!stringBuilder.toString().endsWith("/") && !path.startsWith("/"))
+            stringBuilder.append("/");
+        stringBuilder.append(path);
+
+        vfsHelper.handle(stringBuilder.toString(), file -> {
+            if (file.exists())
+                throw new IllegalStateException("" + file.toString() + " already existing");
+            OutputStream out = file.getContent().getOutputStream();
+            try {
+                StreamUtils.copy(data, out);
+            } catch (IOException e) {
+                throw new FileSystemException(e);
+            } finally {
+                try {
+                    data.close();
+                    out.close();
+                } catch (IOException e) {
+                    log.info("Exception on close stream." + e);
+                }
+            }
+        });
+        return getResource(mode,path);
+    }
+
+    @Override
+    public URI getResource(String mode,String path) throws URISyntaxException {
+        StringBuilder stringBuilder;
+        if(this.huobanmallMode.equals(mode)){
+            stringBuilder = new StringBuilder(huobanmallPrefix.toString());
+        }else {
+            stringBuilder = new StringBuilder(uriPrefix.toString());
+        }
+        if (!stringBuilder.toString().endsWith("/") && !path.startsWith("/"))
+            stringBuilder.append("/");
+        stringBuilder.append(path);
+        return new URI(stringBuilder.toString());
+    }
+
+    @Override
+    public void deleteResource(URI uri) throws IOException {
+        if (!uri.toString().startsWith(uriPrefix.toString())) {
+            log.warn("can not resolve " + uri);
+            return;
+        }
+        String path = uri.toString().substring(uriPrefix.toString().length());
+        deleteResource(path);
+    }
+}
