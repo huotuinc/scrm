785c23bd8f46ad8c1a2cd46f86c8bdd2eb8a37c0
helloztt <347871727@qq.com>
2017-07-17 16:58:23 +0800

    上传图片到商城

--- scrm-common/src/main/java/com/huotu/scrm/common/SysConstant.java
+++ scrm-common/src/main/java/com/huotu/scrm/common/SysConstant.java
@@ -11,11 +11,13 @@ import org.springframework.stereotype.Component;
 public class SysConstant {
     public static String COOKIE_DOMAIN;
     public static String HUOBANMALL_RESOURCE_HOST;
+    public static String HUOBANMALL_PUSH_URL;
 
     @Autowired
     public SysConstant(Environment env){
         COOKIE_DOMAIN = env.getProperty("cookie.domain", ".pdmall.com");
         HUOBANMALL_RESOURCE_HOST = env.getProperty("huobanmall.resourceUrl", "http://res.pdmall.com");
+        HUOBANMALL_PUSH_URL = env.getProperty("huobanmall.pushUrl", "http://mallapi.pdmall.com");
 
     }
 }
