52dc80e826ff355ba52e00e6d26da302d0d254ce
luohaibo <605455337@qq.com>
2017-07-19 11:53:21 +0800

    修改图片的问题

--- scrm-web/src/main/java/com/huotu/scrm/web/controller/mall/InfoController.java
+++ scrm-web/src/main/java/com/huotu/scrm/web/controller/mall/InfoController.java
@@ -1,4 +1,6 @@
 package com.huotu.scrm.web.controller.mall;
+
+
 import com.huotu.scrm.common.utils.InformationSearch;
 import com.huotu.scrm.service.entity.info.Info;
 import com.huotu.scrm.service.service.info.InfoService;
@@ -9,11 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -70,8 +70,6 @@ public class InfoController extends MallBaseController {
         return "info/info_edit";
     }
 
-
-
     /**
      * 保存修改资讯
      * @param info
@@ -85,7 +83,6 @@ public class InfoController extends MallBaseController {
         }
         infoService.infoSave(info);
         return "redirect:/mall/info/infoList";
-
     }
 
 
