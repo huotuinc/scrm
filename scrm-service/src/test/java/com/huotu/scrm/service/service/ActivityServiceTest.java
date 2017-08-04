21e8f6b2a135f51ac141357b92af8247bac6c822
helloztt <347871727@qq.com>
2017-08-04 12:00:47 +0800

    test

--- scrm-service/src/test/java/com/huotu/scrm/service/service/ActivityServiceTest.java
+++ scrm-service/src/test/java/com/huotu/scrm/service/service/ActivityServiceTest.java
@@ -12,15 +12,23 @@ package com.huotu.scrm.service.service;
 import com.huotu.scrm.common.ienum.ActEnum;
 import com.huotu.scrm.common.ienum.EnumHelper;
 import com.huotu.scrm.service.CommonTestBase;
+import com.huotu.scrm.service.entity.activity.ActPrize;
 import com.huotu.scrm.service.entity.activity.Activity;
+import com.huotu.scrm.service.repository.activity.ActPrizeRepository;
+import com.huotu.scrm.service.repository.activity.ActivityRepository;
 import com.huotu.scrm.service.service.activity.ActivityService;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * 活动service层测试类
@@ -31,6 +39,12 @@ public class ActivityServiceTest extends CommonTestBase {
 
     @Autowired
     private ActivityService activityService;
+    @Autowired
+    private ActivityRepository activityRepository;
+    @Autowired
+    private ActPrizeRepository actPrizeRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     /**
      * 测试 分页查询所有活动
@@ -89,6 +103,43 @@ public class ActivityServiceTest extends CommonTestBase {
     public void updateActTest() throws Exception{
         // TODO: 2017-08-04 报错，说明存在漏洞
         activityService.updateActivity(7L);
+    }
+
+    @Test
+    public void deleteActPrize() throws Exception{
+        List<ActPrize> actPrizeList = new ArrayList<>();
+        Activity activity = new Activity();
+
+        ActPrize actPrize1 = new ActPrize();
+        actPrize1.setPrizeName(UUID.randomUUID().toString());
+        actPrize1.setActivity(activity);
+
+        ActPrize actPrize2 = new ActPrize();
+        actPrize2.setPrizeName(UUID.randomUUID().toString());
+        actPrize2.setActivity(activity);
+
+        actPrizeList.add(actPrize1);
+        actPrizeList.add(actPrize2);
+        activity.setActPrizes(actPrizeList);
+        activity = activityRepository.saveAndFlush(activity);
+        //查询
+        Activity realActivity = activityRepository.findOne(activity.getActId());
+        Assert.assertEquals(2,realActivity.getActPrizes().size());
+        Assert.assertEquals(actPrize1.getPrizeName(),realActivity.getActPrizes().get(0).getPrizeName());
+        Assert.assertEquals(actPrize2.getPrizeName(),realActivity.getActPrizes().get(1).getPrizeName());
+
+        //查询actPrize1
+        actPrize1 = actPrizeRepository.findOne(actPrize1.getPrizeId());
+
+        Assert.assertTrue(actPrize1 == realActivity.getActPrizes().get(0));
+
+        //删除actPrize1
+        actPrizeRepository.delete(actPrize1);
+//        entityManager.clear();
+//        entityManager.flush();
+//        entityManager.refresh(realActivity);
+        realActivity = activityRepository.findOne(activity.getActId());
+        Assert.assertEquals(1,realActivity.getActPrizes().size());
     }
 
 }
