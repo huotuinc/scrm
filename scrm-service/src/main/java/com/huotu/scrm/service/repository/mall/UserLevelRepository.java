0913ee59871d829b51cea0818d2b9e2873b4a743
huxuanhao2017 <hu_xuanhao@163.com>
2017-08-10 11:41:05 +0800

    资讯中心修改

--- scrm-service/src/main/java/com/huotu/scrm/service/repository/mall/UserLevelRepository.java
+++ scrm-service/src/main/java/com/huotu/scrm/service/repository/mall/UserLevelRepository.java
@@ -11,21 +11,15 @@ import java.util.List;
 public interface UserLevelRepository extends JpaRepository<UserLevel, Long> {
 
     //根据等级和商户ID查询等级
-    UserLevel findByIdAndCustomerId(Long level, Long customerId);
+    UserLevel findByIdAndCustomerId(Long id, Long customerId);
 
     /**
      * 根据商户id和销售员标记字段，获得等级列表
+     *
      * @param customerId
      * @param isSalesman
      * @return
      */
-    List<UserLevel> findByCustomerIdAndIsSalesman(Long customerId , boolean isSalesman);
+    List<UserLevel> findByCustomerIdAndIsSalesman(Long customerId, boolean isSalesman);
 
-    /**
-     * 根据商户id和Id获得用户等级信息
-     * @param customerId
-     * @param id
-     * @return
-     */
-    UserLevel findByCustomerIdAndId(Long customerId , Long id);
 }
