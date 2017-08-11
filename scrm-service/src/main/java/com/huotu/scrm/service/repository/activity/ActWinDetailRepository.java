bd5dc3dd94f5da5c3f1b30ed7dd9934559d0ba11
luohaibo <605455337@qq.com>
2017-08-11 17:07:51 +0800

    合并代码

--- scrm-service/src/main/java/com/huotu/scrm/service/repository/activity/ActWinDetailRepository.java
+++ scrm-service/src/main/java/com/huotu/scrm/service/repository/activity/ActWinDetailRepository.java
@@ -12,11 +12,11 @@ package com.huotu.scrm.service.repository.activity;
 import com.huotu.scrm.service.entity.activity.ActWinDetail;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 /**
@@ -31,9 +31,10 @@ public interface ActWinDetailRepository extends JpaRepository<ActWinDetail, Long
      * 获取某个人某个活动的所以中奖记录
      * @return
      */
-    List<ActWinDetail> findAllByActIdAndUserId(Long actId,Long userId);
-
+    List<ActWinDetail> findAllByActIdAndUserId(Long actId, Long userId);
 
     Page<ActWinDetail> findAllByActId(Long actId, Pageable pageable);
+
+
 
 }
