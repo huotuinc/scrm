fdbe5459db15a941ff34fe6fe442f6430b2d6de9
luohaibo <605455337@qq.com>
2017-10-26 15:23:35 +0800

    时间问题修改

--- scrm-common/src/main/java/com/huotu/scrm/common/utils/Constant.java
+++ scrm-common/src/main/java/com/huotu/scrm/common/utils/Constant.java
@@ -5,7 +5,7 @@ package com.huotu.scrm.common.utils;
  * Created by helloztt on 2017-07-06.
  */
 public class Constant {
-    public static final int PAGE_SIZE = 20;
+    public static final int PAGE_SIZE = 4;
     public static final String UTF8 = "utf-8";
     /**
      * 默认排名人数
